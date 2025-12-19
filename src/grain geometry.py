a687e3fc489369c2dcb199ec70ee1414a7d36063
guillaumeknight-wq <guillaumeknight@gmail.com>
2025-12-20 00:13:00 +1100

    Update grain geometry.py

--- src/grain geometry.py
+++ src/grain geometry.py
@@ -20,29 +20,29 @@ mech = 'Mevel2017.yaml'
 #Functions
 #solver for Addapted Finocyl surface area, cross section , volume
 def graingeometry_finocyl(geometry_scalar,grainlength,armheight,armwidth,numberofarms,graincentreradius):
-    graincentreradius = graincentreradius + geometry_scalar
-    armwidth = (armwidth + geometry_scalar)
+    graincentreradiusupdate = graincentreradius + geometry_scalar
+    armwidthupdate = (armwidth + (geometry_scalar*2))
     burn_fillet = geometry_scalar
     burn_fillet_area = (burn_fillet**2 - (math.pi*burn_fillet**2))*(numberofarms*2) #accounts for the change in geometery area as the sharp corners burn away
-    angle = math.asin((armwidth/2)/graincentreradius) * 2
-    arclength = angle*2*graincentreradius
-    arcarea = ((graincentreradius**2)*angle/2)-((1/2)*(graincentreradius**2)*math.sin(angle))
+    angle = math.asin((armwidthupdate/2)/graincentreradiusupdate) * 2
+    arclength = angle*2*graincentreradiusupdate
+    arcarea = ((graincentreradiusupdate**2)*angle/2)-((1/2)*(graincentreradiusupdate**2)*math.sin(angle))
     rectanglesubtraction = (grainlength*arclength*numberofarms)
-    boresurfacearea = math.pi*2*graincentreradius*grainlength
-    rectangle_surface_area = (grainlength*(armheight-burn_fillet)*numberofarms*2)+((armwidth-burn_fillet)*grainlength*numberofarms)+(((2 * math.pi* burn_fillet)/4)*grainlength)
+    boresurfacearea = math.pi*2*graincentreradiusupdate*grainlength
+    rectangle_surface_area = (grainlength*(armheight-burn_fillet)*numberofarms*2)+((armwidthupdate-burn_fillet)*grainlength*numberofarms)+(((2 * math.pi* burn_fillet)/4)*grainlength)
     
-    if rectanglesubtraction > boresurfacearea and (armheight+graincentreradius) < chamber_outer_radius :
+    if rectanglesubtraction > boresurfacearea and (armheight+graincentreradiusupdate) < chamber_outer_radius :
         
         grainsurfacearea = rectangle_surface_area
-        graincrosssection = ((3*(3)**(1/2))/2)*(armwidth**2)+(armheight*armwidth*numberofarms)-burn_fillet_area
+        graincrosssection = ((3*(3)**(1/2))/2)*(armwidthupdate**2)+(armheight*armwidthupdate*numberofarms)-burn_fillet_area
         Volumeofgrain = graincrosssection*grainlength
         
     else:
         grainsurfacearea = (boresurfacearea-rectanglesubtraction)+rectangle_surface_area
-        graincrosssection = math.pi*graincentreradius**2+(armheight*armwidth*numberofarms)- arcarea*numberofarms - burn_fillet_area
+        graincrosssection = math.pi*graincentreradiusupdate**2+(armheight*armwidthupdate*numberofarms)- arcarea*numberofarms - burn_fillet_area
         Volumeofgrain = graincrosssection*grainlength
         
-    return(grainsurfacearea,graincrosssection,Volumeofgrain,armheight,armwidth,graincentreradius)    
+    return(grainsurfacearea,graincrosssection,Volumeofgrain,armheight,armwidthupdate,graincentreradiusupdate)    
 
         
     """ #dead code may be needed if calculating area after failure point.
@@ -51,7 +51,7 @@ def graingeometry_finocyl(geometry_scalar,grainlength,armheight,armwidth,numbero
     """    
 
 #input parameters ignore parameters not used in type of grain
-chamber_outer_radius = 51.52/1000 #unless for some strange reason you are making a pressure vessel out of a non round cross section.
+chamber_outer_radius = 51.52/2000 #unless for some strange reason you are making a pressure vessel out of a non round cross section.
 typeofgrain = 'Addapted Finocyl'
 numberofarms = 6 #only used for grains with radial features
 grainlength = 358/1000
@@ -83,11 +83,11 @@ if typeofgrain == 'Addapted Finocyl':
     
     for geometry_scalar in geometry_scalars:
         
-        grainsurfacearea,graincrosssection,volumeofgrain,armheight,armwidth,graincentreradius = graingeometry_finocyl(geometry_scalar, grainlength, armheight, armwidth, numberofarms, graincentreradius)
+        grainsurfacearea,graincrosssection,volumeofgrain,armheight,armwidthupdate,graincentreradiusupdate = graingeometry_finocyl(geometry_scalar, grainlength, armheight, armwidth, numberofarms, graincentreradius)
         surface_area_finocyl.append(grainsurfacearea)
         grain_cross_section_finocyl.append(graincrosssection)
         grain_volume_finocyl.append(volumeofgrain)
-        tracker.append(armwidth)# for troublshooting
+        tracker.append(armwidthupdate)# for troublshooting
     ''' 
     #could not get to work
     y0 = numpy.zeros(5) # = [0,0]
