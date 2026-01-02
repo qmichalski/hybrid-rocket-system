d39d91b9e0460f113b29781ff1b2bfe902c18c85
Arceus2400 <arun24nair@gmail.com>
2026-01-03 00:41:05 +1100

    Update Non_Equilibrium_Tank_Model_ZK.py
    Added volume rate of change of function

--- src/Non_Equilibrium_Tank_Model_ZK.py
+++ src/Non_Equilibrium_Tank_Model_ZK.py
@@ -6,9 +6,10 @@ Created on Fri Jan  2 10:49:22 2026
 """
 
 import CoolProp.CoolProp as CP
+HEOS = CP.AbstractState("HEOS&BICUBIC",'NitrousOxide')
 
-
-def Q_dot_funct(rho,cp,c,n,L,A,g,beta,k,T_fluid, T_surface):
+# %% General Heat Transfer Function between any chosen fluid/solid and surface
+def Q_dot_funct (rho,cp,c,n,L,A,g,beta,k,T_fluid, T_surface):
     delta_T = T_fluid - T_surface
     Ra = cp * (rho**2) * g * beta * abs(delta_T) * (L**3)
     Nu = c*Ra*n
@@ -16,5 +17,26 @@ def Q_dot_funct(rho,cp,c,n,L,A,g,beta,k,T_fluid, T_surface):
     q_dot_exchange = h * A * delta_T
     return q_dot_exchange
 
-def mass_dot_funct():
+# %% Mass flow rate function
     print ('Sample')
+
+# %% Function for rate of change of vapour volume   
+def volume_dot_vap_funct(HEOS, P_tank, m_vap, m_liq, m_dot_vap, m_dot_liq, V_vap, V_liq, T_dot_vap, T_dot_liq):
+    
+    HEOS.update(CP.PQ_INPUTS, P_tank, 0)
+    partial_dP_dT_liq = HEOS.first_partial_deriv(CP.iP, CP.iT, CP.iDmass)
+    partial_dP_drho_liq = HEOS.first_partial_deriv(CP.iP, CP.iDmass, CP.iT)
+    
+    HEOS.update(CP.PQ_INPUTS, P_tank, 1)
+    partial_dP_dT_vap = HEOS.first_partial_deriv(CP.iP, CP.iT, CP.iDmass)
+    partial_dP_drho_vap = HEOS.first_partial_deriv(CP.iP, CP.iDmass, CP.iT)
+
+    combined_term_1 = partial_dP_dT_liq * T_dot_liq + partial_dP_dT_vap * T_dot_vap
+    combined_term_2 = m_dot_liq/V_liq * partial_dP_drho_liq - m_dot_vap/V_vap * partial_dP_drho_vap
+    combined_term_3 = m_liq/(V_liq**2) * partial_dP_drho_liq - m_vap/(V_vap**2) * partial_dP_drho_vap
+    
+    return (combined_term_1 - combined_term_2)/combined_term_3
+
+# %% Function for rate of change of liquid volume
+def volume_dot_liq_funct(V_dot_vap):
+    return -V_dot_vap
