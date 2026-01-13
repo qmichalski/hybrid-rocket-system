be1a1c4f5d985f5ef1ce066b1c68d92dc9caabe0
Arceus2400 <arun24nair@gmail.com>
2026-01-13 19:30:28 +1100

    ZK Tank Model Definitions
    All ZK tank model definitions are added along with a main solver.

--- src/Non_Equilibrium_Tank_Model_ZK.py
+++ src/Non_Equilibrium_Tank_Model_ZK.py
@@ -49,7 +49,7 @@ n_dict = {
 time_step = 0.01
 
 # %% General Heat Transfer Function between any chosen fluid and surface
-def Q_dot_funct (HEOS_from,
+def Heat_dot_funct (HEOS_from,
                  c,
                  n,
                  V,
@@ -84,8 +84,8 @@ def mass_dot_evap_funct (HEOS,
                          V_vap, 
                          V_liq, 
                          T_vap, 
-                         T_fluid, 
-                         E,
+                         T_liq, 
+                         E = 693,
                          CS = CS_tank
                          ):
     
@@ -99,11 +99,11 @@ def mass_dot_evap_funct (HEOS,
     h_vaporization = h_sat_liq - h_sat_vap
     
     HEOS.update(CP.PT_INPUTS, P_tank, T_liq)
-    Q_dot_liq_TO_surf = Q_dot_funct (HEOS, c, n, V_liq, A, g, T_liq, T_surf, CS)*E
+    Q_dot_liq_TO_surf = Heat_dot_funct (HEOS, c["liquid N2O"], n["liquid N2O"], V_liq, A, g, T_liq, T_surf, CS)*E
     h_liq = HEOS.hmass
     
     HEOS.update(CP.PT_INPUTS, P_tank, T_vap)
-    Q_dot_surf_TO_vap = - Q_dot_funct (HEOS, c, n, V_vap, A, g, T_vap, T_surf, CS)
+    Q_dot_surf_TO_vap = - Heat_dot_funct (HEOS, c["vapour N2O"], n["vapour N2O"], V_vap, A, g, T_vap, T_surf, CS)
     
     m_dot_evap = (Q_dot_liq_TO_surf - Q_dot_surf_TO_vap)/ (h_vaporization + h_sat_liq - h_liq)
     
@@ -114,8 +114,8 @@ def mass_dot_cond_funct (HEOS,
                          P_tank, 
                          V_vap, 
                          T_vap, 
-                         T_fluid, 
-                         E,
+                         T_liq, 
+                         E = 693,
                          CS = CS_tank,
                          R_universal = universal_gas_constant,
                          delta_t = time_step):
@@ -135,6 +135,33 @@ def mass_dot_cond_funct (HEOS,
     
     return m_dot_cond
 
+# %% Outlet Mass Flow Rate
+
+def mass_dot_outlet_funct (HEOS, 
+                           Cd, 
+                           P_upstream, 
+                           T_upstream, 
+                           P_downstream):
+    HEOS.update(CP.QT_INPUTS, 0, T_upstream)
+    P_sat = HEOS.P
+    
+    HEOS.update(CP.PT_INPUTS, P_upstream, T_upstream)
+    rho_upstream = HEOS.rhomass
+    h_upstream = HEOS.hmass
+    
+    HEOS.update(CP.PQ_INPUTS, P_downstream, 0)
+    h_downstream = HEOS.hmass
+    rho_downstream = HEOS.rhomass
+    
+    G_SPI = Cd*math.sqrt(2*rho_upstream*(P_upstream - P_downstream))
+    G_HEM = Cd*rho_downstream*math.sqrt(2*(h_upstream - h_downstream))
+    K = math.sqrt((P_upstream - P_downstream)/(P_sat - P_downstream))
+    
+    G = (K*G_SPI + G_HEM) / (K + 1)
+    
+    return G
+    
+
 # %% Function for rate of change of vapour volume   
 def volume_dot_vap_funct (HEOS, 
                          P_tank, 
@@ -142,24 +169,46 @@ def volume_dot_vap_funct (HEOS,
                          m_liq, 
                          m_dot_vap, 
                          m_dot_liq, 
-                         V_vap, V_liq, 
-                         T_vap, T_liq, 
-                         T_dot_vap, 
-                         T_dot_liq):
+                         V_vap, 
+                         V_liq, 
+                         T_vap, 
+                         T_liq,
+                         m_dot_evap, 
+                         m_dot_cond,
+                         m_dot_out,
+                         Q_dot_in_vap,
+                         Q_dot_in_liq):
     
     HEOS.update(CP.PT_INPUTS, P_tank, T_liq)
     partial_dP_dT_liq = HEOS.first_partial_deriv(CP.iP, CP.iT, CP.iDmass)
     partial_dP_drho_liq = HEOS.first_partial_deriv(CP.iP, CP.iDmass, CP.iT)
+    cv_liq = HEOS.cvmass
+    h_evap = HEOS.hmass
+    h_out = HEOS.hmass
     
     HEOS.update(CP.PT_INPUTS, P_tank, T_vap)
     partial_dP_dT_vap = HEOS.first_partial_deriv(CP.iP, CP.iT, CP.iDmass)
     partial_dP_drho_vap = HEOS.first_partial_deriv(CP.iP, CP.iDmass, CP.iT)
+    cv_vap = HEOS.cvmass
+    h_cond = HEOS.hmass
 
-    combined_term_1 = partial_dP_dT_liq * T_dot_liq + partial_dP_dT_vap * T_dot_vap
-    combined_term_2 = m_dot_liq/V_liq * partial_dP_drho_liq - m_dot_vap/V_vap * partial_dP_drho_vap
-    combined_term_3 = m_liq/(V_liq**2) * partial_dP_drho_liq - m_vap/(V_vap**2) * partial_dP_drho_vap
+    f_2 = m_dot_liq/V_liq * partial_dP_drho_liq - m_dot_vap/V_vap * partial_dP_drho_vap
+    f_3 = m_liq/(V_liq**2) * partial_dP_drho_liq - m_vap/(V_vap**2) * partial_dP_drho_vap
     
-    dV_dt_vap = (combined_term_1 - combined_term_2)/combined_term_3
+    f_liq = (-m_dot_out*h_out - m_dot_evap*h_evap + m_dot_cond*h_cond + Q_dot_in_liq)/(m_liq*cv_liq)
+    f_vap = (m_dot_evap*h_evap - m_dot_cond*h_cond + Q_dot_in_vap)/(m_vap*cv_vap)
+    k_liq = P_tank/(m_liq*cv_liq)
+    k_vap = P_tank/(m_vap*cv_vap)
+    
+    #f_1 = partial_dP_dT_liq * T_dot_liq + partial_dP_dT_vap * T_dot_vap #This term has been replaced with subsequent terms to make V_dot independent of T_dot
+    #f_liq = (partial_dP_dT_liq/cv_liq)*(U_dot_liq/m_liq - u_liq*m_dot_liq)
+    #f_vap = (partial_dP_dT_vap/cv_vap)*(U_dot_vap/m_vap - u_vap*m_dot_vap)
+    #g_liq = partial_dP_drho_liq*m_dot_liq/(cv_liq*V_liq)
+    #g_vap = partial_dP_drho_vap*m_dot_vap/(cv_vap*V_vap)
+    #k_liq = - m_liq*partial_dP_drho_liq/(cv_liq*(V_liq**2))
+    #k_vap = - m_vap*partial_dP_drho_vap/(cv_vap*(V_vap**2))
+    
+    dV_dt_vap = (f_liq*partial_dP_dT_liq - f_vap*partial_dP_dT_vap - f_2)/(f_3 - k_liq*partial_dP_dT_liq - k_vap*partial_dP_dT_vap)
     
     return dV_dt_vap
 
@@ -167,9 +216,96 @@ def volume_dot_vap_funct (HEOS,
 def volume_dot_liq_funct(V_dot_vap):
     return -V_dot_vap
 # %% Function for rate of change of fluid phase temperature
-def temperature_dot_funct (HEOS, P_tank, T_phase):
+def temperature_dot_funct (HEOS, 
+                           m_phase, 
+                           m_dot_phase, 
+                           rho_dot_phase, 
+                           P_tank, 
+                           T_phase, 
+                           U_dot_phase
+                           ):
+    
     HEOS.update(CP.PT_INPUTS, P_tank, T_phase)
     cv_phase = HEOS.cvmass
     u_phase = HEOS.umass
     partial_du_drho_phase = HEOS.first_partial_deriv(CP.iUmass, CP.iDmass, CP.iT)
     
+    T_dot_phase = (1/cv_phase)*( (U_dot_phase - u_phase*m_dot_phase)/m_phase - partial_du_drho_phase*rho_dot_phase)
+    
+    return T_dot_phase
+
+# %% Function for rate of change of fluid phase density
+def density_dot_funct(HEOS,
+                      m_phase,
+                      m_dot_phase,
+                      V_phase,
+                      V_dot_phase):
+    rho_dot_phase = m_dot_phase/V_phase - m_phase*V_dot_phase/(V_phase**2)
+    return rho_dot_phase
+
+# %% Function for Overall Liquid Mass Flow Rate
+def mass_dot_LIQ_funct(m_dot_evaporation, 
+                       m_dot_condensation, 
+                       m_dot_exit):
+    
+    #m_dot_liq_overall = mass_dot_cond_funct(HEOS, P_tank, V_vap, T_vap, T_fluid, E, CS = CS_tank, R_universal = universal_gas_constant, delta_t = time_step) - mass_dot_evap_funct(HEOS, c, n, L, A, g, P_tank, V_vap, V_liq, T_vap, T_fluid, E, CS = CS_tank)
+    m_dot_liq_overall = m_dot_condensation - m_dot_evaporation - m_dot_exit
+    return m_dot_liq_overall
+
+# %% Function for Overall Liquid Mass Flow Rate
+def mass_dot_VAP_funct(m_dot_evaporation, 
+                       m_dot_condensation):
+    
+    #m_dot_liq_overall = mass_dot_cond_funct(HEOS, P_tank, V_vap, T_vap, T_fluid, E, CS = CS_tank, R_universal = universal_gas_constant, delta_t = time_step) - mass_dot_evap_funct(HEOS, c, n, L, A, g, P_tank, V_vap, V_liq, T_vap, T_fluid, E, CS = CS_tank)
+    m_dot_vap_overall = m_dot_evaporation - m_dot_condensation
+    return m_dot_vap_overall
+
+# %% Function for Overall Liquid ENERGY Flow Rate
+def energy_dot_vap_funct(HEOS, 
+                         P_tank, 
+                         T_vap, 
+                         T_liq,
+                         m_dot_evap, 
+                         m_dot_cond, 
+                         Q_dot_in_vap, 
+                         V_dot_vap):
+    
+    HEOS.update(CP.PT_INPUTS, P_tank, T_liq)
+    h_evap = HEOS.hmass
+    
+    HEOS.update(CP.PT_INPUTS, P_tank, T_vap)
+    h_cond = HEOS.hmass
+    
+    U_dot_vap = m_dot_evap*h_evap - m_dot_cond*h_cond - P_tank*V_dot_vap + Q_dot_in_vap
+    
+    return U_dot_vap
+
+# %% Function for Overall Liquid ENERGY Flow Rate
+def energy_dot_liq_funct(HEOS, 
+                         P_tank, 
+                         T_vap, 
+                         T_liq, 
+                         V_liq, 
+                         m_dot_evap, 
+                         m_dot_cond, 
+                         m_dot_out, 
+                         Q_dot_in_liq, 
+                         V_dot_liq, 
+                         threshold = 0.01):
+    
+    HEOS.update(CP.PT_INPUTS, P_tank, T_liq)
+    h_evap = HEOS.hmass
+    
+    HEOS.update(CP.PT_INPUTS, P_tank, T_vap)
+    h_cond = HEOS.hmass
+    
+    if V_liq < threshold*V_liq:
+        HEOS.update(CP.PT_INPUTS, P_tank, T_vap)
+        h_out = HEOS.hmass
+    else:
+        HEOS.update(CP.PT_INPUTS, P_tank, T_liq)
+        h_out = HEOS.hmass
+    
+    U_dot_liq = - m_dot_out*h_out - m_dot_evap*h_evap + m_dot_cond*h_cond - P_tank*V_dot_liq + Q_dot_in_liq
+    
+    return U_dot_liq
---
+++ src/Tank_model_solver.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 12 22:08:42 2026
+
+@author: arun2
+"""
+import CoolProp.CoolProp as CP
+import math
+import cantera as ct
+import numpy as np
+import Non_Equilibrium_Tank_Model_ZK as ZK
+
+def tank_model_funct(t,z, # Main Variables
+                     HEOS, threshold, E, CS_tank, c_dict, n_dict, L, A, g, universal_gas_constant): # Args
+
+# %%Unpacking initial data
+    pressure_tank = z[0]
+    temperature_VAP = z[1]
+    temperature_LIQ = z[2]
+    mass_VAP = z[3]
+    mass_LIQ = z[4]
+    #mass_EVAP = z[5]
+    #mass_COND = z[6]
+    #mass_OUT = z[7]
+    #rho_VAP = z[8]
+    #rho_LIQ = z[9]
+    volume_VAP = z[5]
+    volume_LIQ = z[6]
+    #i_energy_VAP = z[12]
+    #i_energy_LIQ = z[13]
+    
+# %%Intermediate parameters
+    dmass_OUT = 0
+    HEOS.update(CP.PQ_INPUTS, P_tank, 0)
+    temperature_surf = HEOS.T
+    
+# %%Mass transfer rate for evaporation and condensation
+    dheat_in_liq = ZK.Heat_dot_funct (HEOS, 
+                                      c_dict["liquid N2O"], 
+                                      n_dict["liquid N2O"], 
+                                      volume_LIQ, 
+                                      A, 
+                                      g, 
+                                      temperature_LIQ, 
+                                      temperature_surf, 
+                                      CS)*E
+    
+    dheat_in_vap = ZK.Heat_dot_funct (HEOS, 
+                                      c_dict["vapour N2O"], 
+                                      n_dict["vapour N2O"], 
+                                      volume_VAP, 
+                                      A, 
+                                      g, 
+                                      temperature_VAP, 
+                                      temperature_surf, 
+                                      CS)
+
+# %%Mass transfer rate for evaporation and condensation
+
+    dmass_EVAP = ZK.mass_dot_evap_funct (HEOS, 
+                             c_dict, 
+                             n_dict, 
+                             L, 
+                             A, 
+                             g, 
+                             pressure_tank, 
+                             volume_VAP, 
+                             volume_LIQ, 
+                             temperature_VAP, 
+                             temperature_LIQ, 
+                             E = 693,
+                             CS = CS_tank)
+    
+    dmass_COND = ZK.mass_dot_cond_funct (HEOS,
+                             pressure_tank, 
+                             volume_VAP,
+                             temperature_VAP, 
+                             temperature_LIQ,
+                             E = 693,
+                             CS = CS_tank,
+                             R_universal = universal_gas_constant,
+                             delta_t = time_step)
+
+# %%Mass transfer rate between the liquid and vapor phases
+    dmass_VAP = ZK.mass_dot_VAP_funct (dmass_EVAP, 
+                                       dmass_COND, 
+                                       dmass_OUT)
+    dmass_LIQ = ZK.mass_dot_LIQ_funct (dmass_EVAP, 
+                                       dmass_COND)
+
+# %%Volume change rate for liquid and vapour phases
+    dvolume_VAP = ZK.volume_dot_vap_funct (HEOS, 
+                                         pressure_tank, 
+                                         mass_VAP, 
+                                         mass_LIQ, 
+                                         dmass_VAP, 
+                                         dmass_LIQ, 
+                                         volume_VAP, 
+                                         volume_LIQ, 
+                                         temperature_VAP, 
+                                         temperature_LIQ,
+                                         dmass_EVAP, 
+                                         dmass_COND,
+                                         dmass_OUT,
+                                         dheat_in_vap,
+                                         dheat_in_liq)
+    
+    dvolume_LIQ = ZK.volume_dot_liq_funct (dvolume_VAP)
+    
+# %%Density change rate for liquid and vapour phases
+    drho_VAP = ZK.density_dot_funct(HEOS, 
+                                    mass_VAP, 
+                                    dmass_VAP, 
+                                    volume_VAP, 
+                                    dvolume_VAP)
+    
+    drho_LIQ = ZK.density_dot_funct(HEOS, 
+                                    mass_LIQ, 
+                                    dmass_LIQ, 
+                                    volume_LIQ, 
+                                    dvolume_LIQ)
+
+# %%Energy transfer rate between the liquid and vapour phases    
+    denergy_VAP = ZK.energy_dot_vap_funct (HEOS, 
+                                           pressure_tank, 
+                                           temperature_VAP, 
+                                           temperature_LIQ, 
+                                           dmass_EVAP, 
+                                           dmass_COND, 
+                                           dheat_in_vap, 
+                                           dvolume_VAP)
+    
+    denergy_LIQ = ZK.energy_dot_liq_funct (HEOS, 
+                                           pressure_tank, 
+                                           temperature_LIQ, 
+                                           dmass_EVAP, 
+                                           dmass_COND, 
+                                           dmass_OUT, 
+                                           dheat_in_liq, 
+                                           dvolume_LIQ, 
+                                           threshold = 0.01)
+    
+# %%Temperature change rate for the liquid and vapor phases
+    dtemperature_VAP = ZK.temperature_dot_funct (HEOS, 
+                                                 mass_VAP, 
+                                                 dmass_VAP,
+                                                 drho_VAP,
+                                                 pressure_tank,
+                                                 temperature_VAP,
+                                                 denergy_VAP)
+    
+    dtemperature_LIQ = ZK.temperature_dot_funct (HEOS, 
+                                                 mass_LIQ, 
+                                                 dmass_LIQ, 
+                                                 drho_LIQ, 
+                                                 pressure_tank, 
+                                                 temperature_LIQ, 
+                                                 denergy_LIQ)
+
+# %%Outputting combined system of ODE
+    dZdt = np.zeros(12)
+    dZdt[0] = dmass_LIQ
+    dZdt[1] = dmass_VAP
+    dZdt[2] = dmass_COND
+    dZdt[3] = dmass_EVAP
+    dZdt[4] = dmass_OUT
+    dZdt[5] = dtemperature_LIQ
+    dZdt[6] = dtemperature_VAP
+    dZdt[7] = denergy_LIQ
+    dZdt[8] = denergy_VAP
+    dZdt[9] = drho_LIQ
+    dZdt[10] = drho_VAP
+    dZdt[11] = dvolume_LIQ
+    dZdt[12] = dvolume_VAP
+    
+    return dZdt
+    
+    
+
+# %% Input Data for Testing
+HEOS = CP.AbstractState("HEOS&BICUBIC",'NitrousOxide')
+
+# Basic Constants
+g = 9.81
+universal_gas_constant = 8.314
+
+# Tank Geometry
+L_tank = 1
+R_tank = 1
+CS = math.pi*(R_tank**2)
+
+V_tank = L_tank*CS
+
+# Initial tank data
+fill_percentage = 90/100
+L_vap = L_tank*(1 - fill_percentage)
+L_liq = L_tank*(fill_percentage)
+
+# Initial Tank conditions
+T_vap = 298.15
+T_liq = 298.15
+P_tank = 56*ct.one_atm
+
+
+# Heat Transfer Constants
+c_dict = {
+    'liquid N2O': 0.15,
+    'vapour N2O': 0.15
+    }
+n_dict = {
+    'liquid N2O': 1/3,
+    'vapour N2O': 1/3
+    }
+
+# Solver Inputs
+time_step = 0.01
